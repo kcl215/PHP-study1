bd757cad3152e04cd8d8b9efa7cc8d6513d52277
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-05-14 11:48:55 +0900

    section2-12 finish

--- sample04.php
+++ sample04.php
@@ -3,3 +3,20 @@ date_default_timezone_set('Asia/Tokyo');
 echo date('G時 i分 s秒');
 ?>
 <br>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo '現在は、';
+echo date('G時 i分 s秒');
+echo 'です';
+?>
+<br>
+<!-- 文字列連結　 -->
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo '現在は、' . date('G時 i分 s秒') . 'です';
+?>
+<br>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo '現在は、' , date('G時 i分 s秒') , 'です';
+?>
