a107c26fb14556c858eb5f77390d3bf05b9108d7
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-05-14 19:05:31 +0900

    section2-19 finish

---
+++ sample11.php
@@ -0,0 +1,19 @@
+<!-- 連想配列とは -->
+<?php
+$fruits = [
+    'apple' => 'りんご',
+    'grape' => 'ぶどう',
+    'lemon' => 'レモン',
+    'tomato' => 'トマト',
+    'peach' => 'もも'
+    ];
+
+// echo $fruits['apple'];
+?>
+
+<dl>
+    <?php foreach ($fruits as $english => $japanese): ?>
+    <dt><?php echo $english; ?></dt>
+    <dd><?php echo $japanese; ?></dd>
+    <?php endforeach; ?>
+</dl>
